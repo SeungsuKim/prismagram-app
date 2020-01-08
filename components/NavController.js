963f6d0943c97a018eea8303dda67c3f076fe26d
SeungsuKim <seungsu0407@gmail.com>
2020-01-08 10:45:29 +0900

    #11.1 AuthNavigation

--- components/NavController.js
+++ components/NavController.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { useIsLoggedIn, useLogIn, useLogOut } from "../AuthContext";
+import AuthNavigation from "../navigations/AuthNavigation";
 
 export default () => {
   const isLoggedIn = useIsLoggedIn();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>NavController</Text>
+    <View style={{ flex: 1 }}>
+      {isLoggedIn ? <Text>Log Out</Text> : <AuthNavigation />}
     </View>
   );
 };
